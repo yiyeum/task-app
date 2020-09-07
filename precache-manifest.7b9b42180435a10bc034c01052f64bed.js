4e19505f1c76dfe03f95822e2b0bfedee12a7b51
Kate YeEum Lee <yeeumm@gmail.com>
2020-09-07 18:58:34 -0400

    Updates

---
+++ precache-manifest.7b9b42180435a10bc034c01052f64bed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac6bf7b8039234deb13621bcc9f5e90",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "05ae74c856b803358139",
+    "url": "/task-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82c1a37e66c858c4381f",
+    "url": "/task-app/static/js/2.0d9e30ce.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task-app/static/js/2.0d9e30ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ae74c856b803358139",
+    "url": "/task-app/static/js/main.9de62221.chunk.js"
+  },
+  {
+    "revision": "9c1264394470c8c681b9",
+    "url": "/task-app/static/js/runtime-main.c5eb4051.js"
+  },
+  {
+    "revision": "e0b71a0eae7d9c559355b83d3ccf20e8",
+    "url": "/task-app/static/media/bg.e0b71a0e.png"
+  }
+]);
