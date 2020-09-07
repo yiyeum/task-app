e3c63b22e33bc4431435ca803fe35bf64440c0fe
Kate YeEum Lee <yeeumm@gmail.com>
2020-09-07 18:39:28 -0400

    fix: remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { TaskView } from './container'
-import { DEFAULT_URL } from './assets/constants/'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path={DEFAULT_URL} exact><TaskView /></Route>
-      </Switch>
-    </Router>
+    <TaskView />
   );
 }
 
